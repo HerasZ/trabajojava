5781ad6682054641a42895145bd42fcf3bdd730a
HerasZ <78933654+HerasZ@users.noreply.github.com>
2021-06-04 18:05:46 +0200

    comienzo interfaz de admin

--- PRUEBAS/src/main/java/Clases/Serie.java
+++ PRUEBAS/src/main/java/Clases/Serie.java
@@ -13,9 +13,9 @@ import java.util.ArrayList;
  */
 public class Serie extends Contenido {
     private int temporadas;
-    private ArrayList<String> capitulos;
+    private ArrayList<Capitulo> capitulos = new ArrayList<Capitulo>();
 
-    public Serie(int temporadas, ArrayList<String> capitulos, String titulo, String sinopsis, String genero, int anno, ArrayList<String> actores) {
+    public Serie(int temporadas, ArrayList<Capitulo> capitulos, String titulo, String sinopsis, String genero, int anno, ArrayList<String> actores) {
         super(titulo, sinopsis, genero, anno, actores);
         this.temporadas = temporadas;
         this.capitulos = capitulos;
@@ -29,11 +29,11 @@ public class Serie extends Contenido {
         this.temporadas = temporadas;
     }
 
-    public ArrayList<String> getCapitulos() {
+    public ArrayList<Capitulo> getCapitulos() {
         return capitulos;
     }
 
-    public void setCapitulos(ArrayList<String> capitulos) {
+    public void setCapitulos(ArrayList<Capitulo> capitulos) {
         this.capitulos = capitulos;
     }
 
