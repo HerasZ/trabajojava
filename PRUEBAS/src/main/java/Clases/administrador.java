f516201c4927b1794232f825dcfd4932d7e5db58
HerasZ <78933654+HerasZ@users.noreply.github.com>
2021-06-13 19:00:38 +0200

    haciendo mas javadoc
    ole

--- PRUEBAS/src/main/java/Clases/administrador.java
+++ PRUEBAS/src/main/java/Clases/administrador.java
@@ -3,6 +3,14 @@ package Clases;
 public class Administrador extends Usuario {
     //atributos
 
+    /**
+     * Constructor de Administrador. Esta clase solo se instancia de forma 
+     * estatica y final en UtilJavaflix
+     * @param correoelectronico correo del Administrador
+     * @param clave clave del administrador
+     * 
+     * @see UtilJavaflix
+     */
     public Administrador(String correoelectronico, String clave) {
         super(clave, correoelectronico);
     }
